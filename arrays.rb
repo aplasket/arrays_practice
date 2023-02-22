a779b33c2fd2731f720227d4803cac146b25c58f
Ashley Plasket <ashley.plasket@gmail.com>
2023-02-22 17:56:08 -0500

    Call length on cohort_members array

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,7 @@ cohort_members = ["KD", "Kim", "Jorja", "Ashley"]
 favorite_numbers = [11, 19, 31, 88]
 local_gas_price = [3.32, 3.41, 2.89, 5.01]
 is_a_student = [false, true, true, false]
+
+# calling length will return number of elements in the cohort_members array
+puts cohort_members.length
+
